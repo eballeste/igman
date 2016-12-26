d0ff114bc6d16b28318ee2d8656ea63d614448a1
Enrique Balleste <eballeste@divisionof.com>
2016-12-26 01:26:32 -0500

    unneeded gulpfile

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-
-gulp.task('watch', function() {
-  browserSync.init({server: {baseDir: './'}});
-  gulp.watch('./scripts/igman.js').on('change', browserSync.reload);
-});
